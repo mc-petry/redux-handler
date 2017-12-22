d61cb19d280d4eda0598401272220b8e3122d44b
mc-petry <Sergii.Petryshyn@simcord.com>
2017-12-22 16:57:36 +0200

    Fix combine handlers

--- src/combine.ts
+++ src/combine.ts
@@ -1,13 +1,28 @@
 import { Reducer, ReducersMapObject, combineReducers } from 'redux'
 import { Handler } from './handler'
 
-export const combineHandlers: <S>(handlers: {[P in keyof S]: Handler<S[P]>}) => Reducer<S> =
-  (handlers) => {
-    const reducers: ReducersMapObject = {}
-
-    for (const handler in handlers) {
-      reducers[handler] = handlers[handler].buildReducer()
-    }
-
-    return combineReducers(reducers)
+export interface Handlers<S> {
+  children: {
+    [P in keyof S]: Handler<S[P]> | Handlers<S[P]>
   }
+  buildReducer(): Reducer<S>
+}
+
+export const combineHandlers: <S>(handlers: {[P in keyof S]: Handler<S[P]> | Handlers<S[P]>}) => Handlers<S> =
+  (handlers) => ({
+    children: handlers,
+    buildReducer: () => {
+      for (const handler in handlers) {
+        const reducers: ReducersMapObject = {}
+        const h = handlers[handler]
+
+        reducers[handler] = typeof (h as Handler<any>).buildReducer === 'function'
+          ? (h as Handler<any>).buildReducer()
+          : (h as Handlers<any>).buildReducer()
+
+        return combineReducers(reducers)
+      }
+
+      return {} as any
+    }
+  })
