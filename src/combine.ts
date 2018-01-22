382bd4a36f09b90e946220053cfe8b2dcd3f7676
mc-petry <Sergii.Petryshyn@simcord.com>
2018-01-22 16:08:11 +0200

    Extend buildReducer with other reducers

--- src/combine.ts
+++ src/combine.ts
@@ -1,18 +1,18 @@
-import { Reducer, ReducersMapObject, combineReducers } from 'redux'
+import { Reducer, combineReducers } from 'redux'
 import { Handler } from './handler'
 
 export interface Handlers<S> {
   children: {
     [P in keyof S]: Handler<S[P]> | Handlers<S[P]>
   }
-  buildReducer(): Reducer<S>
+  buildReducer<RS>(other?: {[P in keyof RS]: Reducer<RS>}): Reducer<S & RS>
 }
 
 export const combineHandlers: <S>(handlers: {[P in keyof S]: Handler<S[P]> | Handlers<S[P]>}) => Handlers<S> =
   (handlers) => ({
     children: handlers,
-    buildReducer: () => {
-      const reducers: ReducersMapObject = {}
+    buildReducer: <RS>(other?: {[P in keyof RS]: Reducer<RS> }) => {
+      const reducers: { [key: string]: Reducer<any> } = other || {}
 
       for (const handler in handlers) {
         const h = handlers[handler]
@@ -22,6 +22,17 @@ export const combineHandlers: <S>(handlers: {[P in keyof S]: Handler<S[P]> | Han
           : (h as Handlers<any>).buildReducer()
       }
 
-      return combineReducers(reducers)
+      return combineReducers(reducers) as any
     }
+  })
+
+interface TestStore {
+  store: { test?: string }
+}
+
+combineHandlers<TestStore>({
+  store: new Handler<{ test?: string }>({})
+})
+  .buildReducer({
+
   })
